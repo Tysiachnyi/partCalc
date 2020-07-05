033d5a8aeda592915660a39738d5f1e3198b0e91
Nikita Tysiachnyi <nikita.tysiachnyi@extreme-idea.com>
2020-07-05 17:24:00 +0300

    added enter key fnc

--- index.js
+++ index.js
@@ -10,19 +10,35 @@ document.getElementById('checkVal').addEventListener('click', () => {
   console.log(arr);
   let val =
     `
-  <div class="row">
-        <div class="center-align">
-          <div class="card-panel indigo">
-        <span class="white-text">
+  
         ${arr}
-        </span>
-          </div>
-        </div>
-      </div>
+        
   `;
 
   document.getElementById('arrayVal').innerHTML = val;
 });
+
+document.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    let num = document.getElementById('Value').value;
+    let maxNum = 4950;
+
+    let arr = Array.from({length: Math.floor(num/maxNum)}, () => maxNum);
+    let left = Number(num%maxNum).toFixed(2).toString().replace(".", ",");
+    if(left !== 0) {
+      arr.push(left);
+    }
+    console.log(arr);
+    let val =
+      `
+  
+        ${arr}
+        
+  `;
+
+    document.getElementById('arrayVal').innerHTML = val;
+  }
+})
 
 
 if (document.getElementById('checkVal').value.length === 0 ) {
